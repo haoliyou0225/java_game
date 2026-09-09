46426abbc8077c7654aef5aca0b64b9dfbb38255
hly <1278714255@qq.com>
2026-09-09 15:49:19 +0800

    添加了对应表格的类名(2)

--- src/main/java/com.dz.hello.Main/MainApp.java
+++ src/main/java/com.dz.hello.Main/MainApp.java
@@ -12,9 +12,7 @@ import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
